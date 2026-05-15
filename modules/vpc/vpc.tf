b8fe76163debd10cdcde1f1be10cfcd317f397d8
BhupeshDahiya <bhupesh40rajput@gmail.com>
2026-05-16 02:18:54 +0530

    VPC module created

---
+++ modules/vpc/vpc.tf
@@ -0,0 +1,101 @@
+resource "aws_vpc" "gitops_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  # Required for EKS and private node communication
+  enable_dns_support   = true 
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "gitops-vpc"
+  }
+}
+
+# making only 1 private and 1 public subnet for demonstration purposes
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.gitops_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "gitops-public"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.gitops_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "gitops-private"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.gitops_vpc.id
+
+  tags = {
+    Name = "gitops igw"
+  }
+}
+
+# Elastic IP for the NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "gitops NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# Public rt 
+
+resource "aws_route_table" "pub_rt" {
+  vpc_id = aws_vpc.gitops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "gitops-pub-rt"
+  }
+}
+
+# Private rt
+
+resource "aws_route_table" "pvt_rt" {
+  vpc_id = aws_vpc.gitops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "gitops-pvt-rt"
+  }
+}
+
+resource "aws_route_table_association" "pub" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.pub_rt.id
+}
+
+resource "aws_route_table_association" "pvt" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.pvt_rt.id
+}
